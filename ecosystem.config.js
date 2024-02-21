f9d52e73bebba5214c5da8034f7ccdcb57c7af49
Sambit <majhisambit2@gmail.com>
2024-02-21 11:22:59 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,8 @@ module.exports = {
     apps: [{
         name: "app1",
         script: "dhttpd",
-        args: "--path outputs/web/"
+        args: "--path outputs/web/",
+        interpreter : "/snap/bin/dart",
+        watch: true
     }]
 }
